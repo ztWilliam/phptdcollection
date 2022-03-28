2c34fc0ba291a6882d6d287d82a4818b2fb4192b
will-linww <linww@zhengtuo.net>
2022-03-28 11:55:42 +0800

    Add options support to the ITdConnection interface.

--- src/Connector/ITdConnection.php
+++ src/Connector/ITdConnection.php
@@ -14,10 +14,11 @@ interface ITdConnection{
      * @param String $user 用户名
      * @param String $pass 密码
      * @param String $defaultDb 默认Db Name，可选
+     * @param array $options 可通过 Key-Value 数组，对连接选项进行初始化
      * 
      * @return ITdConnection | null 若连接成功，返回一个 ITdConnection对象，若连接失败，则返回null
      */
-    public static function connect(String $host, String $port, String $user, String $pass, String $defaultDb = '');
+    public static function connect(String $host, String $port, String $user, String $pass, String $defaultDb = '', array $options = []);
 
     /**
      * 销毁连接相关信息，
@@ -36,6 +37,16 @@ interface ITdConnection{
      * @return ITdConnection 将自身对象返回，以便支持 -> 链式调用
      */
     public function withDefaultDb(String $dbName) : ITdConnection;
+
+    /**
+     * 在执行命令时，动态指定连接的选项。
+     * 被指定的选项，将会在连接对象存续期间持续生效。
+     * 
+     * @param array $options 需要修改的选项的 key-value 数组
+     * 
+     * @return ITdConnection 将自身对象返回，以便支持 -> 链式调用
+     */
+    public function withOptions(array $options) : ITdConnection;
 
     /**
      * 执行非查询类的操作命令。
