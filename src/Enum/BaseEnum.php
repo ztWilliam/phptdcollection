cefda782feb7e84ef0610a3f944982585ce93fac
will-linww <linww@zhengtuo.net>
2022-03-29 16:13:52 +0800

    Add data types enum class . The enum available after php 8 isn't used , for  support the users who uses older php version.

---
+++ src/Enum/BaseEnum.php
@@ -0,0 +1,35 @@
+<?php 
+namespace WztzTech\Iot\PhpTd\Enum;
+
+abstract class BaseEnum {
+    private static $constCacheArray = NULL;
+
+    public static function getKeyMap() {
+        if (self::$constCacheArray == NULL) {
+            self::$constCacheArray = [];
+        }
+        $calledClass = get_called_class();
+        if (!array_key_exists($calledClass, self::$constCacheArray)) {
+            $reflect = new \ReflectionClass($calledClass);
+            self::$constCacheArray[$calledClass] = $reflect->getConstants();
+        }
+        return self::$constCacheArray[$calledClass];
+    }
+
+    public static function isValidName($name, $strict = false) {
+        $constants = self::getKeyMap();
+
+        if ($strict) {
+            return array_key_exists($name, $constants);
+        }
+
+        $keys = array_map('strtolower', array_keys($constants));
+        return in_array(strtolower($name), $keys);
+    }
+
+    public static function isValidValue($value, $strict = true) {
+        $values = array_values(self::getKeyMap());
+        return in_array($value, $values, $strict);
+    }
+
+}
